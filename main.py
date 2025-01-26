69d3add1583060c32450d3e31680c605e08b19be
Sh0bra <jshin1@cpp.edu>
2025-01-25 23:24:00 -0800

    changed double quotes to single

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen heigth: {SCREEN_HEIGHT}")
+    print('Starting asteroids!')
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
 
 if __name__ == "__main__":
     main()
