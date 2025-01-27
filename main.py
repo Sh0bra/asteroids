d84696fffbf2598ab77392467c1523b148857903
Sh0bra <jshin1@cpp.edu>
2025-01-26 21:04:14 -0800

    using groups instead of obj

--- main.py
+++ main.py
@@ -12,19 +12,30 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
-    p1 = Player(x, y)
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        p1.update(dt)       
-        screen.fill("black")
-        p1.draw(screen)
-        pygame.display.flip()
+            
         
+        for obj in updateable:
+            obj.update(dt)
+
+        screen.fill("black")
+        
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
