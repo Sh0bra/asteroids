844b4636112affb5747f1f87f9a88ee3faded4e3
Sh0bra <jshin1@cpp.edu>
2025-01-26 20:44:19 -0800

    added roatating features to player

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        p1.update(dt)       
         screen.fill("black")
-        pygame.display.flip()
         p1.draw(screen)
+        pygame.display.flip()
+        
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
